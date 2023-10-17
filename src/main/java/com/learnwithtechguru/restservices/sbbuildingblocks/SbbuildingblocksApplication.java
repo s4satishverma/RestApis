23a9d85a84efc6fde7a4338f9890f308ab615f95
Satish <s4satishverma@gmail.com>
2023-10-16 20:21:38 -0400

    First Commit

---
+++ src/main/java/com/learnwithtechguru/restservices/sbbuildingblocks/SbbuildingblocksApplication.java
@@ -0,0 +1,14 @@
+package com.learnwithtechguru.restservices.sbbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbuildingblocksApplication.class, args);
+		System.out.println("Hello World...");
+	}
+
+}
---
+++ src/test/java/com/learnwithtechguru/restservices/sbbuildingblocks/SbbuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithtechguru.restservices.sbbuildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
